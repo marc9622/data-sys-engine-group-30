2c1d3ede577adbbca5cfd24e97c7c1e8952e7af0
marc9622 <maaw@itu.dk>
2026-09-15 16:02:56 +0200

    Added more parsing tests

--- src/test/java/dk/itu/datasys/SqlParserTest.java
+++ src/test/java/dk/itu/datasys/SqlParserTest.java
@@ -57,7 +57,7 @@ class SqlParserTest {
     }
 
     @Test
-    void caseInsensitiveKeywords() {
+    void parsesCaseInsensitiveStatements() {
         SqlParserFacade parser = new SqlParserFacade();
 
         List<Statement> statements = parser.parse(
@@ -74,6 +74,55 @@ class SqlParserTest {
         assertEquals(new Statement.Select("trips",
                 Optional.of(
                     new Statement.Select.Predicate("distance", Comparison.GREATER_THAN, 100L)
+                )),
+                statements.get(2));
+    }
+
+    @Test
+    void parsesStatementsWithComments() {
+        SqlParserFacade parser = new SqlParserFacade();
+
+        List<Statement> statements = parser.parse(
+                "-- This is a comment\n" +
+                "CREATE TABLE trips (\n" +
+                "   city STRING, -- comment STRING\n" + 
+                "   distance LONG,\n" +
+                "   price DOUBLE\n" +
+                "); -- Another comment\n" +
+                "COPY trips FROM 'trips.csv';\n" +
+                "SELECT * FROM trips WHERE distance > 100; -- Final comment\n");
+
+        assertEquals(3, statements.size());
+        assertEquals(new Statement.CreateTable("trips", List.of(
+                new ColumnSpec("city", ColumnType.STRING),
+                new ColumnSpec("distance", ColumnType.LONG),
+                new ColumnSpec("price", ColumnType.DOUBLE))), statements.get(0));
+        assertEquals(new Statement.Copy("trips", "trips.csv"), statements.get(1));
+        assertEquals(new Statement.Select("trips",
+                Optional.of(
+                    new Statement.Select.Predicate("distance", Comparison.GREATER_THAN, 100L)
+                )),
+                statements.get(2));
+    }
+
+    @Test
+    void parsesNegativeNumbers() {
+        SqlParserFacade parser = new SqlParserFacade();
+
+        List<Statement> statements = parser.parse(
+                "CREATE TABLE trips (city STRING, distance LONG, price DOUBLE);" +
+                "COPY trips FROM 'trips.csv';" +
+                "SELECT * FROM trips WHERE distance < -100;");
+
+        assertEquals(3, statements.size());
+        assertEquals(new Statement.CreateTable("trips", List.of(
+                new ColumnSpec("city", ColumnType.STRING),
+                new ColumnSpec("distance", ColumnType.LONG),
+                new ColumnSpec("price", ColumnType.DOUBLE))), statements.get(0));
+        assertEquals(new Statement.Copy("trips", "trips.csv"), statements.get(1));
+        assertEquals(new Statement.Select("trips",
+                Optional.of(
+                    new Statement.Select.Predicate("distance", Comparison.LESS_THAN, -100L)
                 )),
                 statements.get(2));
     }
